5bfeeccdc1f2194f8c8623a338b914fa320f3aac
Mateus-Henr <mateush.study@gmail.com>
2022-06-27 01:59:21 -0300

    Add MakeFile

---
+++ build/CMakeFiles/TP1-AEDS2.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/matt/Programming/University/Basic-Search-Engine/src/file/file.c" "/home/matt/Programming/University/Basic-Search-Engine/build/CMakeFiles/TP1-AEDS2.dir/src/file/file.c.o"
+  "/home/matt/Programming/University/Basic-Search-Engine/src/hashtable/hashtable.c" "/home/matt/Programming/University/Basic-Search-Engine/build/CMakeFiles/TP1-AEDS2.dir/src/hashtable/hashtable.c.o"
+  "/home/matt/Programming/University/Basic-Search-Engine/src/linkedlist/linkedlist.c" "/home/matt/Programming/University/Basic-Search-Engine/build/CMakeFiles/TP1-AEDS2.dir/src/linkedlist/linkedlist.c.o"
+  "/home/matt/Programming/University/Basic-Search-Engine/src/linkedlist/node.c" "/home/matt/Programming/University/Basic-Search-Engine/build/CMakeFiles/TP1-AEDS2.dir/src/linkedlist/node.c.o"
+  "/home/matt/Programming/University/Basic-Search-Engine/src/main.c" "/home/matt/Programming/University/Basic-Search-Engine/build/CMakeFiles/TP1-AEDS2.dir/src/main.c.o"
+  "/home/matt/Programming/University/Basic-Search-Engine/src/pairlinkedlist/pairlinkedlist.c" "/home/matt/Programming/University/Basic-Search-Engine/build/CMakeFiles/TP1-AEDS2.dir/src/pairlinkedlist/pairlinkedlist.c.o"
+  "/home/matt/Programming/University/Basic-Search-Engine/src/pairlinkedlist/pairnode.c" "/home/matt/Programming/University/Basic-Search-Engine/build/CMakeFiles/TP1-AEDS2.dir/src/pairlinkedlist/pairnode.c.o"
+  "/home/matt/Programming/University/Basic-Search-Engine/src/patricia/patricia.c" "/home/matt/Programming/University/Basic-Search-Engine/build/CMakeFiles/TP1-AEDS2.dir/src/patricia/patricia.c.o"
+  "/home/matt/Programming/University/Basic-Search-Engine/src/patricia/treenode.c" "/home/matt/Programming/University/Basic-Search-Engine/build/CMakeFiles/TP1-AEDS2.dir/src/patricia/treenode.c.o"
+  "/home/matt/Programming/University/Basic-Search-Engine/src/tfidf/tfidf.c" "/home/matt/Programming/University/Basic-Search-Engine/build/CMakeFiles/TP1-AEDS2.dir/src/tfidf/tfidf.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TP1-AEDS2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP1-AEDS2.dir/src/file/file.c.o"
+  "CMakeFiles/TP1-AEDS2.dir/src/hashtable/hashtable.c.o"
+  "CMakeFiles/TP1-AEDS2.dir/src/linkedlist/linkedlist.c.o"
+  "CMakeFiles/TP1-AEDS2.dir/src/linkedlist/node.c.o"
+  "CMakeFiles/TP1-AEDS2.dir/src/main.c.o"
+  "CMakeFiles/TP1-AEDS2.dir/src/pairlinkedlist/pairlinkedlist.c.o"
+  "CMakeFiles/TP1-AEDS2.dir/src/pairlinkedlist/pairnode.c.o"
+  "CMakeFiles/TP1-AEDS2.dir/src/patricia/patricia.c.o"
+  "CMakeFiles/TP1-AEDS2.dir/src/patricia/treenode.c.o"
+  "CMakeFiles/TP1-AEDS2.dir/src/tfidf/tfidf.c.o"
+  "TP1-AEDS2"
+  "TP1-AEDS2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TP1-AEDS2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
